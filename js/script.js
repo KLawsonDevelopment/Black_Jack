60e7776c1958eba28d2c73ff5a9d1ae0af6dc314
Keith <ozmaneku@gmail.com>
2019-02-15 07:32:54 -0500

    Changed minor coded

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
     console.log("jQuery active")
 
-    // var deck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11];
-    var deck = [11,11,11,11]
+    var deck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11];
     console.log(deck);
     var shuffledDeck = _.shuffle(deck);
     console.log(shuffledDeck);
